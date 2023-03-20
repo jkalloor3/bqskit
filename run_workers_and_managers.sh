369cdef0eadbc9c798081a8d13a43e8e79ebe4ba
Alon Kukliansky <alonkukl@gmail.com>
2023-03-20 16:53:39 -0700

    more prints

--- run_workers_and_managers.sh
+++ run_workers_and_managers.sh
@@ -4,8 +4,6 @@ amount_of_gpus=$1
 amont_of_workers_per_gpu=$2
 total_amount_of_workers=$(($amount_of_gpus * $amont_of_workers_per_gpu))
 
-
-
 if [ $amount_of_gpus -eq 0 ]
 then
     echo "Will run manager on node $node_id with n args of $amont_of_workers_per_gpu"
@@ -14,6 +12,8 @@ then
 else
     echo "Starting MPS servers on node $node_id"
     nvidia-cuda-mps-control -d
+
+    echo "Starting $total_amount_of_workers workes on $amount_of_gpus gpus"
     for (( gpu_id=0; gpu_id<$amount_of_gpus; gpu_id++ ))
     do
     XLA_PYTHON_CLIENT_PREALLOCATE=false CUDA_VISIBLE_DEVICES=$gpu_id bqskit-worker $amont_of_workers_per_gpu &> $SCRATCH/bqskit_logs/workers_${SLURM_JOB_ID}_${node_id}_${gpu_id}.log &
@@ -21,6 +21,7 @@ else
 
     echo "Will run manager on node $node_id"
     bqskit-manager -x -n$total_amount_of_workers -v &> $SCRATCH/bqskit_logs/manager_${SLURM_JOB_ID}_${node_id}.log
+    echo "Manager finished on node $node_id" >> $SCRATCH/bqskit_logs/manager_${SLURM_JOB_ID}_${node_id}.log
     echo "Manager finished on node $node_id"
 
 
