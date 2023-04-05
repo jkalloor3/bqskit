8fb545df283a5453d7bb9b4baf07f8fb51b0dfab
Alon Kukliansky <alonkukl@gmail.com>
2023-04-05 11:22:01 -0700

    More stable executions

--- create_runs.py
+++ create_runs.py
@@ -12,24 +12,34 @@ detached_runtime_header="""#!/bin/bash
 #SBATCH -A m4141_g
 #SBATCH -C gpu
 #SBATCH -q regular
-#SBATCH -t 11:44:00
+#SBATCH -t 11:50:00
 #SBATCH -n {nodes}
 #SBATCH --mem=0
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task={amount_of_gpus_per_node}
-#SBATCH --output=./logs/{cir_name}_{partitions_size}p_{instantiator}_detached_runtime_{nodes}_nodes_{num_multistarts}_starts-%j.txt
+#SBATCH --output=./logs_tol_a_0/{cir_name}_{partitions_size}p_{instantiator}_detached_runtime_{nodes}_nodes_{num_multistarts}_starts-%j.txt
 
 date
 uname -a
 module load python
 conda activate my_env
+module load nvidia
+
+
 
 echo "starting BQSKit managers on all nodes"
 srun run_workers_and_managers.sh {amount_of_gpus_per_node} {amount_of_workers_per_gpu} &
 managers_pid=$!
 
-while [[ ! -f $SCRATCH/managers_${{SLURM_JOB_ID}}_started ]]
+filename=$SCRATCH/managers_${{SLURM_JOB_ID}}_started
+n={nodes}
+
+while [[ ! -f "$filename" ]]
 do
+        sleep 0.5
+done
+
+while [ "$(cat "$filename" | wc -l)" -lt "$n" ]; do
     sleep 1
 done
 
@@ -37,9 +47,11 @@ echo "starting BQSKit server on main node"
 bqskit-server $(scontrol show hostnames "$SLURM_JOB_NODELIST" | tr '\\n' ' ') &> $SCRATCH/bqskit_logs/server_${{SLURM_JOB_ID}}.log &
 server_pid=$!
 
-echo "will run {env_vars} python  ./{python_file} {command_args} "
+uname -a >> $SCRATCH/server_${{SLURM_JOB_ID}}_started
 
-{env_vars} python  ./{python_file} --use_detached {command_args}
+echo "will run {env_vars} python  ./{python_file} --diff_tol_a 0  --use_detached {command_args} "
+
+{env_vars} python  ./{python_file} --diff_tol_a 0 --use_detached {command_args}
 
 
 echo "Killing the server"
@@ -55,24 +67,25 @@ attached_runtime_header="""#!/bin/bash
 #SBATCH -A m4141_g
 #SBATCH -C gpu
 #SBATCH -q regular
-#SBATCH -t 11:44:00
+#SBATCH -t 0:20:00
 #SBATCH -n {nodes}
 #SBATCH --mem=0
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task={amount_of_gpus_per_node}
-#SBATCH --output=./logs/{cir_name}_{partitions_size}p_{instantiator}_attached_runtime_{nodes}_nodes_{num_multistarts}_starts-%j.txt
+#SBATCH --output=./logs_tol_a_0/{cir_name}_{partitions_size}p_{instantiator}_attached_runtime_{nodes}_nodes_{num_multistarts}_starts-%j.txt
 
 date
 uname -a
 module load python
 conda activate my_env
+module load nvidia
 
 echo "starting MPS server on node"
 nvidia-cuda-mps-control -d
 
-echo "will run {env_vars} python  ./{python_file} {command_args} "
+echo "will run {env_vars} python  ./{python_file}  --diff_tol_a 0 {command_args} "
 
-{env_vars} python  ./{python_file} {command_args}
+{env_vars} python  ./{python_file}  --diff_tol_a 0 {command_args}
 
 sleep 1
 echo "Trying to stop MPS on all nodes"
@@ -110,6 +123,12 @@ def create_and_run_a_job(cir_path, partitions_size, python_file,
         command_args += f' --amount_of_workers {workers_per_node}'
         command_args += f' --amount_gpus_per_node 0'
 
+    if amount_of_gpus_per_node == 0:
+
+        amount_of_workers_per_gpu = workers_per_node
+    else:
+        amount_of_workers_per_gpu = workers_per_node // amount_of_gpus_per_node
+
 
     if use_detached:
         runtime_type='detached'
@@ -127,7 +146,8 @@ def create_and_run_a_job(cir_path, partitions_size, python_file,
                         env_vars=env_vars, command_args=command_args,
                         nodes = nodes, amount_of_gpus_per_node=amount_of_gpus_per_node,
                         workers_per_node=workers_per_node, python_file=python_file,
-                        runtime_type=runtime_type, amount_of_workers_per_gpu=workers_per_node//amount_of_gpus_per_node))
+                        runtime_type=runtime_type,
+                                        amount_of_workers_per_gpu=amount_of_workers_per_gpu))
 
     to_write.close()
     time.sleep(2*sleep_time)
@@ -144,18 +164,30 @@ if __name__ == '__main__':
     # circuits =  [('qaoa5.qasm', 5), ('grover5_u3.qasm', 5), ('adder9_u3.qasm', 9), ('hub4.qasm', 4)]
     # circuits =  [ ('qaoa12_u3.qasm', 12)]
     # circuits =  [ ('adder63_u3.qasm', 63), ('shor26.qasm', 26), ('hub18.qasm', 18)]
-    circuits =  [ ('hub4.qasm', 4)]
+    # circuits =  [ ('adder63_u3.qasm', 63)]
+    # circuits =  [ ('hub4.qasm', 4)]
+    circuits =  [ ('hub18.qasm', 18)]
+    # circuits =  [ ('shor26.qasm', 26)]
+    # circuits =  [ ('grover5_u3.qasm', 5)]
+    # circuits =  [ ('qaoa5.qasm', 5)]
+    # circuits =  [ ('adder9_u3.qasm', 9)]
 
+    # instantiators = ['CERES', 'QFACTOR-RUST', 'LBFGS']
     # instantiators = ['CERES', 'QFACTOR-RUST', 'QFACTOR-JAX', 'LBFGS']
+    # instantiators = ['CERES', 'QFACTOR-RUST', 'QFACTOR-JAX']
+    # instantiators = ['CERES', 'QFACTOR-RUST',  'LBFGS']
     # instantiators = ['CERES', 'QFACTOR-RUST']
-    # instantiators = ['QFACTOR-JAX', 'QFACTOR-RUST']
-    instantiators = ['QFACTOR-RUST']
-    # instantiators = ['LBFGS']
+    # instantiators = ['CERES', 'QFACTOR-JAX']
+    # instantiators = ['CERES']
+    # instantiators = ['QFACTOR-JAX']
+    # instantiators = ['QFACTOR-RUST']
+    # instantiators = ['QFACTOR-RUST', 'LBFGS']
+    instantiators = ['LBFGS']
 
-    # partisions_size_l = [8,7]
-    partisions_size_l = [4]
-    # partisions_size_l = [3,4,5,6,7,8, 9, 10, 11, 12]
-    # partisions_size_l = [10, 11, 12]
+    partisions_size_l = [4,5, 6]
+    #partisions_size_l = [3,4,5,6,7,8]
+    # partisions_size_l = [3,4,5,6,7,8, 9,11,12]
+    # partisions_size_l = [5, 6, 7,8, 9]
     # partisions_size_l = [13, 14, 15]
 
     num_multistarts_l = [32]
@@ -164,16 +196,17 @@ if __name__ == '__main__':
 
 
     # n_nodes = [4, 2, 1]
-    n_nodes = [1]
-    # n_nodes = [4]
+    # n_nodes = [1]
+    n_nodes = [8]
 
     n_workers_per_node = [-1]
-    n_amount_of_gpus_in_node=[1]
+    n_amount_of_gpus_in_node=[0]
 
-    use_detached = False
-    # use_detached = True
+    # use_detached = False
+    use_detached = True
 
     python_file = 'gate_deletion_perf_measurement.py'
+    # python_file = 'gate_deletion_perf_measurement_test.py'
 
     for amount_of_gpus_per_node in n_amount_of_gpus_in_node:
         for workers_per_node in n_workers_per_node:
--- run_workers_and_managers.sh
+++ run_workers_and_managers.sh
@@ -4,7 +4,8 @@ amount_of_gpus=$1
 amont_of_workers_per_gpu=$2
 total_amount_of_workers=$(($amount_of_gpus * $amont_of_workers_per_gpu))
 
-date > $SCRATCH/managers_${SLURM_JOB_ID}_started
+touch $SCRATCH/managers_${SLURM_JOB_ID}_started
+uname -a >> $SCRATCH/managers_${SLURM_JOB_ID}_started
 
 if [ $amount_of_gpus -eq 0 ]
 then
@@ -18,7 +19,16 @@ else
     echo "Starting MPS servers on node $node_id"
     nvidia-cuda-mps-control -d
 
-    sleep 4 #Need to wait for the server to connect to the managers
+    
+
+    #Need to wait for the server to connect to the managers
+    i=0
+    while [[ ! -f $SCRATCH/server_${SLURM_JOB_ID}_started && $i -lt 10 ]]
+    do
+        sleep 1
+        i=$((i+1))
+    done
+
 
     echo "Starting $total_amount_of_workers workes on $amount_of_gpus gpus"
     for (( gpu_id=0; gpu_id<$amount_of_gpus; gpu_id++ ))
@@ -36,3 +46,5 @@ else
     echo "Stop MPS servers on node $node_id"
     echo quit | nvidia-cuda-mps-control
 fi
+
+df -hl /tmp >> $SCRATCH/bqskit_logs/manager_${SLURM_JOB_ID}_${node_id}.log
