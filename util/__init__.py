c083e9c06d0f7f5978a13ea9508b6e09823c50db
Justin Kalloor <justin.kalloor@gmail.com>
2025-01-14 14:56:28 -0800

    Fixing checkpoint issue

--- util/__init__.py
+++ util/__init__.py
@@ -22,3 +22,4 @@ from .ensemble_scan import EnsembleScanningGateRemovalPass
 from .check_ensemble_quality import CheckEnsembleQualityPass
 from .jiggle_scans import JiggleScansPass
 from .jiggle_ensemble import JiggleEnsemblePass
+from .ensemble import CreateEnsemblePass
