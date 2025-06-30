212c0a7535bc62c000a3613f7c3b99618a97a623
Justin Kalloor <justin.kalloor@gmail.com>
2025-06-30 09:53:28 -0700

    Modifying GG Test

--- test_gg_qp.py
+++ test_gg_qp.py
@@ -6,6 +6,7 @@ from bqskit.compiler.passdata import PassData
 from itertools import chain, product
 from util.generate_probs_pass import GenerateProbabilityPass
 import numpy as np
+# import time
 from bqskit.ir.gates import CNOTGate, U3Gate
 from bqskit.qis import UnitaryMatrix
 from math import ceil
@@ -14,26 +15,137 @@ from bqskit.ir.gates import RZGate
 from bqskit.passes import ScanPartitioner, SetTargetPass
 from bqskit.compiler import Compiler
 from bqskit.ir.circuit import Circuit, CircuitGate, CircuitPoint
-from util.distance import frobenius_cost, normalized_gp_frob_cost
+from util.distance import frobenius_cost, normalized_gp_frob_cost, gp_frobenius_cost
 from util.common import load_block, load_ensemble
-from util.distance import get_corrected_un
+from util.distance import get_corrected_un, tvd
 import csv
 from bqskit.ir.lang.qasm2 import OPENQASM2Language
 from bqskit.ir.opt.cost.functions import GPNormalizedFrobeniusCostGenerator, GPNormalizedFrobeniusCostGenerator
+import cvxpy as cp
+import cvxopt
+from qpsolvers import solve_qp
 
-circ_name = "qpe10"
-block_name = "2"
+circ_name = "qae13"
+block_name = "3"
 tol = 3.0
 ind = 0
-small_block = "00"
+small_block = "0"
 
 
-qasms_file = f"/pscratch/sd/j/jkalloor/bqskit/small_block_checkpoints_final_paper_4_clifft_tket/{circ_name}_{block_name}_{tol}/block_{small_block}/ensemble_0_.qasms"
+qasms_file = f"small_block_checkpoints_final_paper_4_clifft_tket/{circ_name}_{block_name}_{tol}/block_{small_block}/ensemble_0_.qasms"
 jiggle_file = f"small_block_checkpoints_final_paper_4_clifft_tket/{circ_name}_{block_name}_{tol}/block_{small_block}/ensemble_0_jiggles_.npy"
-cache_file = f"/pscratch/sd/j/jkalloor/bqskit/small_block_checkpoints_final_paper_4_clifft_tket/{circ_name}_{block_name}_{tol}/block_{small_block}/ensemble_0_cache.pkl"
+cache_file = f"small_block_checkpoints_final_paper_4_clifft_tket/{circ_name}_{block_name}_{tol}/block_{small_block}/ensemble_0_cache.pkl"
 
 frob_cost = GPNormalizedFrobeniusCostGenerator()
 lang = OPENQASM2Language(gate_defs=[("gg", gg_gate_def)])
+
+
+def calculate_probs_qp(H: np.ndarray, f: np.ndarray) -> np.ndarray[float]:
+    M = H.shape[0]
+    # Constraints, probabilities should sum to 1 and be between 0 and 1
+    Aeq = np.ones((1, M))
+    beq = np.array([1])
+    lbound = np.zeros(M)
+    ubound = np.ones(M)
+
+    xk = solve_qp(H, f, A=Aeq, b=beq, lb=lbound, ub=ubound, solver='clarabel')                    
+
+    return xk
+
+def calculate_H_f(ensemble: np.ndarray, target: UnitaryMatrix) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Calculate the Hessian and gradient for the Frank-Wolf algorithm.
+    """
+    M = ensemble.shape[0]
+        
+    tr_V_Us = np.einsum("mij,ij->m", ensemble, target.conj(), optimize=True)
+    tr_Us = np.einsum("aij,bij->ab", ensemble.conj(), ensemble, optimize=True)
+
+    f = -2 * np.real(tr_V_Us)
+    H = 2 * np.real(tr_Us)
+
+    # Make pos definite
+    evs = np.linalg.eigvals(H)
+    isposdef = np.all(evs > 0)
+    trials = 0
+    while not isposdef and trials < 20:
+        H += 1e-10 * np.eye(M)
+        print(f"Perturbing a little to make pos def try #: {trials}")
+        evs = np.linalg.eigvals(H)
+        isposdef = np.all(evs > 0)
+        trials += 1
+
+    if not isposdef:
+        print('H not positive definite by a lot! Returning uniform dist')
+        return None, None
+    
+    return H, f
+
+def calculate_probs_frank_wolf(H: np.ndarray, f: np.ndarray,
+                               initial_xk: np.ndarray = None) -> np.ndarray[float]:
+    
+    M = H.shape[0]
+    if initial_xk is None:
+        xk = np.ones(M) / M
+    else:
+        xk = initial_xk
+    nSteps = 20
+    # temp = np.zeros((4, nSteps))
+
+    # Constraints, probabilities should sum to 1 and be between 0 and 1
+    Aeq = np.ones((1, M))
+    beq = np.array([1])
+    lbound = np.zeros(M)
+    ubound = np.ones(M)
+
+    for _ in range(nSteps):
+        DelJxk = H @ xk + f
+
+        # Solve: min_y DelJxk.T @ y  s.t. Aeq @ y = beq, lbound <= y <= ubound
+        # Using scipy.linprog for efficiency
+        y_var = cp.Variable(M)
+        lp_obj = cp.Minimize(DelJxk @ y_var)
+        lp_constraints = [Aeq @ y_var == beq, y_var >= lbound, y_var <= ubound]
+        lp_prob = cp.Problem(lp_obj, lp_constraints)
+        lp_prob.solve(solver=cp.CVXOPT) 
+
+        y = y_var.value
+        step = y - xk
+
+        # Optimal step size (gamma_star)
+        numerator = -step @ DelJxk
+        denominator = step @ H @ step
+        gamma_star = min(1.0, numerator / denominator) if denominator > 1e-12 else 1.0
+
+        # Update
+        xk = xk + gamma_star * step
+        print("diff: ", np.linalg.norm(gamma_star * step), flush=True)
+    return xk
+
+def calculate_bias_var_covar(uns: list[UnitaryMatrix], probs: list[float],
+                             target: UnitaryMatrix):
+    '''
+    Calculate the bias, var, and covariance error terms
+    '''
+    avg_un = np.average(uns, axis=0, weights=probs)
+    bias_err = gp_frobenius_cost(avg_un, target)
+
+    var_err = 0
+    for p, un in zip(probs,uns):
+        var_err += (p ** 2) * (gp_frobenius_cost(avg_un, un) ** 2)
+
+    covar_err = 0
+    for i, (pi, ui) in enumerate(zip(probs, uns)):
+        for j, (pj, uj) in enumerate(zip(probs, uns)):
+            if i == j:
+                continue
+            u1 = ui - avg_un
+            u2 = uj - avg_un
+            covar_err += (pi * pj) * np.trace(u1.conj().T @ u2)
+
+    covar_err = 2 * np.real(covar_err)
+
+    return bias_err, var_err, covar_err
 
 def random_gg_circ(num_qubits: int, num_ggs: int) -> Circuit:
     """
@@ -74,26 +186,32 @@ class CreateProbEnsemble(BasePass):
         """
         uns = []
         probs = []
-        new_circ = circ.copy()
         # print(f"Generating unitaries for {len(gg_inds)} selections", flush=True)
         # print(f"GG Indices: {gg_inds}", flush=True)
         for i, selections in enumerate(gg_inds):
             gg_ind = 0
+            new_circ = circ.copy()
             gg_prob = [gg_probs[j][selection] for j, selection in enumerate(selections)]
             gg_strs = [gg_param_options[j][selection] for j, selection in enumerate(selections)]
             total_prob = np.prod(gg_prob)
             # if total_prob < 1e-6:
             #     continue
+            # print("Selections: ", selections, "Total Prob: ", total_prob, flush=True)
             for cycle, op in new_circ.operations_with_cycles():
                 if isinstance(op.gate, GridSynthGate):
                     t_str= gg_strs[gg_ind]
                     t_circ = gridsynth_gates_to_cir(t_str)
                     pt = CircuitPoint(cycle, op.location[0])
                     new_circ.replace_with_circuit(pt, t_circ, as_circuit_gate=True)
+                    # rz_cost = frob_cost.calc_cost(t_circ, RZGate().get_unitary([op.params[0]]))
+                    # print(f"Replaced GG at ({cycle}, qubit {op.location[0]}) w/"
+                    #       f"RZ Cost: {rz_cost}, Prob: {gg_prob[gg_ind]}", flush=True)
                     gg_ind += 1
                     if gg_ind >= len(selections):
                         # Only modify first `limit` GGs
                         break
+            # full_cost = frob_cost.calc_cost(new_circ, target)
+            # print(f"Full Cost for circ {i+1}/{len(gg_inds)}: {full_cost}, Total Prob: {total_prob}", flush=True)
             uns.append(get_corrected_un(new_circ.get_unitary(), target))
             probs.append(total_prob)
 
@@ -131,22 +249,15 @@ class CreateProbEnsemble(BasePass):
                 gg_circs = [gridsynth_gates_to_cir(s) for s in gg_strs]
                 gg_uns = [c.get_unitary() for c in gg_circs]
                 gg_uns = [get_corrected_un(u, rz_target) for u in gg_uns]
-                avg_dist = np.mean([frob_cost(c, rz_target) for c in gg_circs])
-                avg_un = np.average(gg_uns, axis=0, weights=probs)
-                avg_un_dist = normalized_gp_frob_cost(avg_un, rz_target)
-                gamma = avg_un_dist / (avg_dist ** 2)
-                print(f"Angle: {angle}, Avg Dist: {avg_dist}, "
-                      f"Avg Unitary Dist: {avg_un_dist}, "
-                      f"Gamma: {gamma}", flush=True)
                 gg_param_options.append(gg_strs)
                 gg_probs.append(probs)
-                if len(gg_probs) > self.limit:
+                if len(gg_probs) >= self.limit:
                     break
 
         # Chunk into 1000 groups
         all_gg_inds = list(product(range(4), repeat=len(gg_probs)))
         # print(f"Total number of selections: {len(all_gg_inds)}", flush=True)
-        all_gg_inds = np.array_split(all_gg_inds, 256)
+        all_gg_inds = np.array_split(all_gg_inds, 128)
         # print(f"Total number of selections: {len(all_gg_inds)}", flush=True)
 
         all_results = await get_runtime().map(
@@ -166,93 +277,96 @@ class CreateProbEnsemble(BasePass):
 
 if __name__ == '__main__':
 
-    orig_circ = load_block("qpe10", "2", "_tket")
-    orig_circ = Circuit.from_file(orig_circ)
-    # print(orig_circ.gate_counts, flush=True)
+    compiler = Compiler(num_workers=128)
 
-    compiler = Compiler(num_workers=256)
-    part_circ = compiler.compile(orig_circ, [ScanPartitioner(4)])
-    block_target_circs = {}
-    num_digits = len(str(part_circ.num_operations))
-    for i, op in enumerate(part_circ.operations()):
-        assert isinstance(op.gate, CircuitGate)
-        label = str(i).zfill(num_digits)
-        block_target_circs[label] = op.gate._circuit
-    target_circ: Circuit = block_target_circs[small_block]
-    target = target_circ.get_unitary()
-
-    ensemble = load_ensemble(qasms_file)
-    circ = ensemble[int(small_block)]
     csv_data = []
-    for gg_num in range(3, 8):
-        # 4 random circuits for each number of GridSynthGates
-        # for _ in range(4):
-        # num_ggs = circ.count(GridSynthGate())
-        num_cnots = circ.count(CNOTGate())
-        num_u3s = circ.count(U3Gate())
+    for gg_num in range(4, 6):
+        # Random Circ
+        for _ in range(3):
+            circ = random_gg_circ(4, gg_num)
+            target = circ.get_unitary()
+            num_cnots = circ.count(CNOTGate())
+            num_u3s = circ.count(U3Gate())
 
-        dist = frob_cost.calc_cost(circ, target)
+            _, data = compiler.compile(circ, [
+                SetTargetPass(target),
+                CreateProbEnsemble(success_threshold=(10 ** (-tol)),
+                                limit=10),], 
+                request_data=True)
 
-        _, data = compiler.compile(circ, [
-            SetTargetPass(target),
-            CreateProbEnsemble(success_threshold=(10 ** (-tol)),
-                               limit=gg_num),], 
-            request_data=True)
+            uns = data['uns']
+            probs = data['probs']
+                
+            # Normalize probabilities
+            probs = np.array(probs)
+            if np.sum(probs) > 0:
+                probs /= np.sum(probs)
 
-        uns = data['uns']
-        probs = data['probs']
+            # Pick 10000 random unitaries from the ensemble
+            NUM_CIRCS_PER_PROB = 4096
+            if len(uns) > NUM_CIRCS_PER_PROB:
+                rand_un_inds = np.random.choice(len(uns), size=NUM_CIRCS_PER_PROB, 
+                                                replace=False)
+                uns = [uns[i] for i in rand_un_inds]
+                dists = [dists[i] for i in rand_un_inds]
+                probs = [probs[i] for i in rand_un_inds]
+
+            dists = [gp_frobenius_cost(u, target) for u in uns]
+
+            ensemble = np.array(uns)
+            H, f = calculate_H_f(ensemble, target)
+
+            # Method 1: Uniform Probabilities
+            uniform_probs = np.ones(len(uns)) / len(uns)
+            # Calculate the bias, var, and covar errors
+            bias_err, var_err, covar_err = calculate_bias_var_covar(uns, uniform_probs, target)
+            avg_dist = np.mean(dists)
+            gamma = bias_err / (avg_dist ** 2) 
+
+            # Method 2: GG Probabilities
+            bias_err_gg, var_err_gg, covar_err_gg = calculate_bias_var_covar(uns, probs, target)
+            avg_dist_gg = np.average(dists, weights=probs)
+            gamma_gg = bias_err_gg / (avg_dist_gg ** 2)
+
+            # Calculate Probs with QP
+            ensemble = np.array(uns)
+            qp_probs = calculate_probs_qp(H, f)
+
+            bias_err_qp, var_err_qp, covar_err_qp = calculate_bias_var_covar(uns, qp_probs, target)
+            avg_dist_qp = np.average(dists, weights=qp_probs)
+            gamma_qp = bias_err_qp / (avg_dist_qp ** 2)
+
+            #Frank-Wolf algorithm
+            fw_probs = calculate_probs_frank_wolf(H, f)
+            bias_err_fw, var_err_fw, covar_err_fw = calculate_bias_var_covar(uns, fw_probs, target)
+            avg_dist_fw = np.average(dists, weights=fw_probs)
+            gamma_fw = bias_err_fw / (avg_dist_fw ** 2)
             
-        # Normalize probabilities
-        probs = np.array(probs)
-        if np.sum(probs) > 0:
-            probs /= np.sum(probs)
+            # Frank-Wolf with seeded probabilities
+            fws_probs = calculate_probs_frank_wolf(H, f, initial_xk=probs)
+            bias_err_fws, var_err_fws, covar_err_fws = calculate_bias_var_covar(uns, fws_probs, target) 
+            avg_dist_fws = np.average(dists, weights=fws_probs)
+            gamma_fws = bias_err_fws / (avg_dist_fws ** 2)
 
-        dists = [normalized_gp_frob_cost(u, target) for u in uns]
-        avg_dist_1 = np.mean(dists)
-        # print("Avg Distance: ", avg_dist)
-
-        avg_un = np.average(uns, axis=0, weights=probs)
-
-        avg_un_dist_1 = normalized_gp_frob_cost(avg_un, target)
-        # print("Avg Unitary Distance: ", avg_un_dist)
-
-        gamma = avg_un_dist_1 / (avg_dist_1 ** 2)
-        # print("Scaling factor gamma: ", gamma, flush=True) 
-
-        # Pick 10000 random unitaries from the ensemble
-        NUM_CIRCS_PER_PROB = 4000
-        if len(uns) > NUM_CIRCS_PER_PROB:
-            rand_un_inds = np.random.choice(len(uns), size=NUM_CIRCS_PER_PROB, 
-                                            replace=False)
-            uns = [uns[i] for i in rand_un_inds]
-            dists = [dists[i] for i in rand_un_inds]
-        
-        avg_dist_qp = np.mean(dists)
-
-        ensemble = np.array(uns)
-
-        qp_probs = GenerateProbabilityPass.calculate_probs(ensemble, target=target)
-
-        avg_un_qp = np.average(ensemble, axis=0, weights=qp_probs)
-        avg_un_qp_dist = normalized_gp_frob_cost(avg_un_qp, target)
-        # print("Avg Unitary QP Distance: ", avg_un_qp_dist)
-
-        gamma_qp = avg_un_qp_dist / (avg_dist_qp ** 2)
-        # print("Scaling factor gamma QP: ", gamma_qp)
-        row = [gg_num, num_cnots, num_u3s, avg_dist_1, avg_un_dist_1, gamma, 
-            avg_dist_qp, avg_un_qp_dist, gamma_qp]
-        
-        print(row, flush=True)
-
-        csv_data.append(row)
+            row = [avg_dist, len(uns), bias_err, var_err + covar_err, gamma,
+                   bias_err_gg, var_err_gg + covar_err_gg, gamma_gg,
+                   bias_err_qp, var_err_qp + covar_err_qp, gamma_qp,
+                   bias_err_fw, var_err_fw + covar_err_fw, gamma_fw,
+                   bias_err_fws, var_err_fws + covar_err_fws, gamma_fws]
+            csv_data.append(row)
 
     compiler.close()
     # Write data to csv
-    headers = ["Num GGs", "Num CNOTs", "Num U3s", "Avg. Dist",
-               "Avg. Unitary Dist", "Gamma", "Avg. Dist QP",
-               "Avg. Unitary Dist QP", "Gamma QP"]
-    with open('gg_qp_results_qpe.csv', 'w', newline='') as f:
+    headers = ["Epsilon", "Num Circs", 
+               "Bias Err (Uniform)", "Var Err + Covar Err (Uniform)", "Scaling Factor (Uniform)",
+               "Bias Err GG", "Var Err + Covar Err GG", "Scaling Factor GG",
+               "Bias Err QP", "Var Err + Covar Err QP", "Scaling Factor QP", 
+               "Bias Err FW", "Var Err + Covar Err FW", "Scaling Factor FW",
+               "Bias Err FW Seeded", "Var Err + Covar Err FW Seeded", "Scaling Factor FW Seeded"
+               ]
+    csv_file_name = 'gg_qp_all_results_rand_circs.csv'
+    with open(csv_file_name, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(headers)
         writer.writerows(csv_data)
-        print("Data written to gg_qp_results.csv", flush=True)
+        print("Data written to ", csv_file_name, flush=True)
