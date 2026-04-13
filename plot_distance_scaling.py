005067cf7b78d2e9f3519fedddf65d013e946ab4
Justin Kalloor <justin.kalloor@gmail.com>
2026-04-13 11:59:29 -0700

    Plotting Other Hamiltonians

--- plot_distance_scaling.py
+++ plot_distance_scaling.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
         
     cliff_t = True
     cliff_t_string = "_clifft" if cliff_t else ""
-    folder_form = "ensemble_dms_{circ_name}" + cliff_t_string + "_final_2/"
+    folder_form = "ensemble_dms_{circ_name}" + cliff_t_string + "_final{extra}/"
 
     fig, axs = plt.subplots(1, 1, figsize=(10, 6))
 
@@ -33,9 +33,11 @@ if __name__ == '__main__':
     else:
         y_label = "Hamiltonian Observable Error of Channel"
 
-    plot_dm_data(circ_names, axs, folder_form=folder_form, y_label=y_label, calc_obs=not TRACE_DISTANCE, cliff_t=cliff_t)
+    plot_dm_data(circ_names, axs, folder_form=folder_form, y_label=y_label, 
+                 calc_obs=not TRACE_DISTANCE, cliff_t=cliff_t, 
+                 extras=["_N", "_Sz"])
 
     if TRACE_DISTANCE:
         plt.savefig('trace_distance_scaling_clifft_final.png', dpi=300)
     else:
-        plt.savefig('hamiltonian_scaling_clifft_final.png', dpi=300)
+        plt.savefig('other_hamiltonian_scaling_clifft_final.png', dpi=300)
